eb0785c4f7864314cb6a52c893ad54aa4abd79f9
xieyun <xieyun@pagoda.com.cn>
2020-12-22 16:28:45 +0800

    强化force判断

--- modules/config.js
+++ modules/config.js
@@ -1,5 +1,5 @@
 module.exports = (function(){
-  const forceFlux = process.env.FORCEFlux || true
+  const forceFlux = (process.env.FORCEFlux === 'true') || false
   const fluxHost = process.env.fluxHost || 'http://119.29.74.239:8081'
   const fitHost = process.env.fitHost || "http://39.108.1.180:7022"
   const whs4Fit = process.env.WAREHOUSENO4Fit || 'A2'
